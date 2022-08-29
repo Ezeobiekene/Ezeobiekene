cc72ae466daee999328a0fae13e2893e44fbea40
Ezeobiekene <95988883+Ezeobiekene@users.noreply.github.com>
2022-08-28 22:06:59 -0400

    Update Names.java

--- Ezeobiekene/Names.java
+++ Ezeobiekene/Names.java
@@ -1,5 +1,3 @@
-package com.git.names;
-
 public class Names{
 
     //obtain the total milliseconds since Midnight, Jan 1 1970
@@ -7,9 +5,20 @@ public class Names{
         long totalMilliseconds = System.currentTimeMillis();
 
         //total seconds
-        long totalsecond = totalMilliseconds / 1000;
+        long totalSeconds = totalMilliseconds / 1000;
 
         long  currentSecond = (int) (totalSeconds % 60);
+
+        long totalMinutes = totalSeconds / 60;
+
+        long currentMinute = (int) (totalMinutes % 60);
+        
+        long totalHours = totalMinutes / 60;
+
+        long currentHour = (int) (totalHours % 24);
+
+        System.out.println("Current time is  " + currentHour + ":"
+        + currentMinute + ":" + currentSecond + " GMT");
     }
 
 }
